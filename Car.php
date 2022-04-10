fdd159f5053488ebb7e4620feea11986a4527266
DangirasH <97556742+DangirasH@users.noreply.github.com>
2022-04-10 13:20:36 +0200

    Add files via upload

---
+++ Car.php
@@ -0,0 +1,91 @@
+<?php
+
+
+require_once 'Vehicle.php';
+
+
+class Car extends Vehicle
+
+{
+    public const  ALLOWED_ENERGIES = [
+        "fuel",
+        "electric"
+    ];
+
+    protected bool $hasParkBrake;
+    protected string $energy;
+    protected int $energyLevel;
+
+
+    public function __construct(string $color, int $nbSeats, string $energy)
+    {
+        parent::__construct($color, $nbSeats);
+
+        $this->setEnergy($energy);
+    }
+
+
+    public function start(): string
+    {
+        if($this->getHasParkBrake() === true){
+            throw new Exception('Warning : Park brake on!');
+        }
+        else {
+            $this->currentSpeed = 15;
+            return 'Car srarted !';
+        }
+    }
+    
+
+
+
+/* Getter Setter */
+
+    public function getEnergy(): string
+    {
+
+        return $this->energy;
+    }
+
+    public function getEnergyLevel(): int
+
+    {
+
+        return $this->energyLevel;
+    }
+
+    public function setEnergyLevel(int $energyLevel): void
+
+    {
+
+        $this->energyLevel = $energyLevel;
+    }
+
+
+    public function setEnergy(string $energy): Car
+
+    {
+
+        if (in_array($energy, self::ALLOWED_ENERGIES)) {
+
+            $this->energy = $energy;
+        }
+
+        return $this;
+    }
+    
+    public function getHasParkBrake(): bool
+    {
+
+        return $this->hasParkBrake;
+    }
+
+    public function setHasParkBrake(bool $hasParkBrake): void
+
+    {
+
+        $this->hasParkBrake = $hasParkBrake;
+
+    }
+
+}
